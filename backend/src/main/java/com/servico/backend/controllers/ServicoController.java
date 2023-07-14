94d1115bb17a03e802ea881d6b0978bf5ec2b2fd
Esmyrna <esmyrnaoliveira13@gmail.com>
2023-07-13 23:17:21 -0300

    cors

--- backend/src/main/java/com/servico/backend/controllers/ServicoController.java
+++ backend/src/main/java/com/servico/backend/controllers/ServicoController.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class ServicoController {
     @Autowired
     private ServicoService servicoService;
-
+    @CrossOrigin(origins = "http://localhost:3000/")
     @GetMapping
     public List<Servico> findAll() {
         return servicoService.findAll();
@@ -29,20 +29,24 @@ public class ServicoController {
     public List<Servico> findCanceledServices() {
         return servicoService.findCanceledServices();
     }
+    @CrossOrigin(origins = "http://localhost:3000/")
     @PostMapping("/")
     public Servico insert(@RequestBody  Servico servico){
         return servicoService.insert(servico);
     }
+
     @PostMapping("/{id}")
     public ResponseEntity<Void> cancel(@PathVariable Long id){
         servicoService.cancelService(id);
         return ResponseEntity.ok().build();
     }
+    @CrossOrigin(origins = "http://localhost:3000/")
     @PutMapping("/{id}")
     public Servico toAlter(@PathVariable Long id ,@RequestBody  Servico servico){
         return servicoService.toAlter(id, servico);
     }
 
+    @CrossOrigin(origins = "http://localhost:3000/")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> removed(@PathVariable Long id) {
         servicoService.delete(id);
